ae64a004d89dc814ea418a4b577b410cb362b76c
deanwraith <dean.wraith@gmail.com>
2021-07-31 09:36:06 +0000

    Added additional styling

--- README.md
+++ README.md
@@ -84,6 +84,8 @@ The devices used were:
     Added script for scissors, lizard and spock outcomes. Added to README.md
     * 09:32 31/07/2021 - CSS and README
     Added media queries for responsiveness. Added to README.md
+    * 11:35 31/07/2021 - CSS and README
+    Added styling to elements. Added to README.md
     
 
 ## 5. Technology Used
--- assets/js/script.js
+++ assets/js/script.js
@@ -42,115 +42,115 @@ const game = () => {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Rock smashes Scissors, You Win! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[3]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Rock crushes Lizard, You Win!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 // Rock Loses.
             } else if (showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[1]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Paper covers Rock, You Lose! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[4]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Spock vapourizes Rock, You Lose!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 //Paper outcomes
                 //Paper wins
             } else if (showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[0]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Paper covers Rock, You Win! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[4]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Paper disproves Spock, You Win!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 // Paper Loses.
             } else if (showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[2]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Scissors cuts Paper, You Lose! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[3]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Lizard eats Paper, You Lose!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 //Scissors outcomes
                 //Scissors wins
             } else if (showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[1]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Scissors cuts Paper, You Win! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[3]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Scissors decapitates Lizard, You Win!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 // Scissors Loses.
             } else if (showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[0]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Rock smashes Scissors, You Lose! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[4]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Spock smashes Scissors, You Lose!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 //Lizard outcomes
                 //Lizard wins
             } else if (showIcon.className === randomClasses[3] && computerShowIcon.className === randomClasses[1]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Lizard eats Paper, You Win! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[3] && computerShowIcon.className === randomClasses[4]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Lizard poisens Spock, You Win!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 // Lizard Loses.
             } else if (showIcon.className === randomClasses[3] && computerShowIcon.className === randomClasses[0]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Rock cruches Lizard, You Lose! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[3] && computerShowIcon.className === randomClasses[2]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Scissors decapitates Lizard, You Lose!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 //Spock outcomes
                 //Spock wins
             } else if (showIcon.className === randomClasses[4] && computerShowIcon.className === randomClasses[0]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Spock vapourizes Rock, You Win! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[4] && computerShowIcon.className === randomClasses[2]) {
                 pScore.innerHTML = userscore;
                 userscore++;
                 text.innerHTML = "Spock smashes scissors, You Win!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
                 // Spock Loses.
             } else if (showIcon.className === randomClasses[4] && computerShowIcon.className === randomClasses[1]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Paper disproves Spock, You Lose! ";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             } else if (showIcon.className === randomClasses[4] && computerShowIcon.className === randomClasses[3]) {
                 cScore.innerHTML = compscore;
                 compscore++;
                 text.innerHTML = "Lizard poisens Spock, You Lose!";
-                text.style.color = 'red';
+                text.style.color = 'orange';
             }
         });
     });
